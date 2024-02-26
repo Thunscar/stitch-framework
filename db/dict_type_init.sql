3f03fb1a345c1a20d38adbce2fa25ae23a674318
486484645@qq.com <486484645@qq.com>
2024-02-26 16:12:33 +0800

    提交字典类型数据库sql文件

---
+++ db/dict_type_init.sql
@@ -0,0 +1,5 @@
+insert into sys_dict_type values (1,'用户性别','user_gender','0','stitch',sysdate(),'',sysdate(),'用户性别列表','0');
+insert into sys_dict_type values (2,'菜单可用状态','menu_status','0','stitch',sysdate(),'',sysdate(),'菜单可用状态列表','0');
+insert into sys_dict_type values (3,'菜单展示状态','menu_display','0','stitch',sysdate(),'',SYSDATE(),'菜单展示状态列表','0');
+insert into sys_dict_type values (4,'系统通用状态','normal_status','0','stitch',sysdate(),'',SYSDATE(),'系统通用状态列表','0');
+insert into sys_dict_type values (5,'系统是否','yes_no','0','stitch',sysdate(),'',SYSDATE(),'系统是否列表','0');
