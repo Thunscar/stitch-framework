6da32eb1e248c322533bca2e7bac1e18370438db
486484645@qq.com <486484645@qq.com>
2024-03-30 22:03:26 +0800

    优化请求接口

--- stitch-web/src/main/java/com/stitchcodes/web/controller/system/SysRoleController.java
+++ stitch-web/src/main/java/com/stitchcodes/web/controller/system/SysRoleController.java
@@ -7,8 +7,10 @@ import com.stitchcodes.common.controller.BaseController;
 import com.stitchcodes.common.enums.BusinessType;
 import com.stitchcodes.common.excel.ExcelUtil;
 import com.stitchcodes.common.utils.ObjectUtils;
+import com.stitchcodes.core.domain.SysDept;
 import com.stitchcodes.core.domain.SysRole;
 import com.stitchcodes.core.domain.model.LoginUser;
+import com.stitchcodes.core.service.SysDeptService;
 import com.stitchcodes.core.service.SysRoleService;
 import com.stitchcodes.core.utils.AuthUtils;
 import com.stitchcodes.system.service.SysPermissionService;
@@ -36,6 +38,8 @@ public class SysRoleController extends BaseController {
     private SysPermissionService permissionService;
     @Resource
     private SysTokenService tokenService;
+    @Resource
+    private SysDeptService deptService;
 
     //查询角色列表
     @PreAuthorize("@pm.hasPerms('sys:role:list')")
@@ -57,12 +61,16 @@ public class SysRoleController extends BaseController {
     //查询角色的数据权限 自定义数据权限会返回可访问的部门列表
     @GetMapping("/data/{roleId}")
     public AjaxResult getDataScope(@PathVariable Long roleId) {
-        //检查角色的访问权限
-        return success(roleService.selectSysRoleContainsDataScope(roleId));
+        AjaxResult result = new AjaxResult();
+        SysRole sysRole = roleService.selectSysRoleContainsDataScope(roleId);
+        List<SysDept> deptList = deptService.selectSysDeptList(new SysDept());
+        result.put("role", sysRole);
+        result.put("depts", deptList);
+        return result;
     }
 
     //批量删除角色
-    @Log(title = "删除角色",BusinessType = BusinessType.DELETE)
+    @Log(title = "删除角色", BusinessType = BusinessType.DELETE)
     @PreAuthorize("@pm.hasPerms('sys:role:delete')")
     @DeleteMapping("/{roleIds}")
     public AjaxResult remove(@PathVariable Long[] roleIds) {
@@ -80,7 +88,7 @@ public class SysRoleController extends BaseController {
     }
 
     //创建角色
-    @Log(title = "创建角色",BusinessType = BusinessType.INSERT)
+    @Log(title = "创建角色", BusinessType = BusinessType.INSERT)
     @PreAuthorize("@pm.hasPerms('sys:role:create')")
     @PostMapping
     public AjaxResult create(@RequestBody SysRole role) {
@@ -94,7 +102,7 @@ public class SysRoleController extends BaseController {
     }
 
     //更新角色信息
-    @Log(title = "更新角色",BusinessType = BusinessType.UPDATE)
+    @Log(title = "更新角色", BusinessType = BusinessType.UPDATE)
     @PreAuthorize("@pm.hasPerms('sys:role:update')")
     @PutMapping
     public AjaxResult update(@RequestBody SysRole role) {
@@ -118,7 +126,7 @@ public class SysRoleController extends BaseController {
     }
 
     //导出角色信息到excel
-    @Log(title = "导出角色",BusinessType = BusinessType.EXPORT)
+    @Log(title = "导出角色", BusinessType = BusinessType.EXPORT)
     @PreAuthorize("@pm.hasPerms('sys:role:export')")
     @PostMapping("/export")
     public void export(SysRole role, HttpServletResponse response) throws IOException {
@@ -128,7 +136,7 @@ public class SysRoleController extends BaseController {
     }
 
     //给角色分配用户
-    @Log(title = "角色分配给多个用户",BusinessType = BusinessType.OTHER)
+    @Log(title = "角色分配给多个用户", BusinessType = BusinessType.OTHER)
     @PreAuthorize("@pm.hasPerms('sys:role:allocatedUser')")
     @PostMapping("/allocate")
     public AjaxResult allocateUsers(Long roleId, Long[] userIds) {
@@ -138,7 +146,7 @@ public class SysRoleController extends BaseController {
     }
 
     //取消角色分配的用户
-    @Log(title = "角色取消分配给多个用户",BusinessType = BusinessType.OTHER)
+    @Log(title = "角色取消分配给多个用户", BusinessType = BusinessType.OTHER)
     @PreAuthorize("@pm.hasPerms('sys:role:allocatedUser')")
     @PostMapping("/allocate/cancel")
     public AjaxResult cancelAllocateUsers(Long roleId, Long[] userIds) {
@@ -148,7 +156,7 @@ public class SysRoleController extends BaseController {
     }
 
     //修改角色数据权限
-    @Log(title = "修改角色数据权限",BusinessType = BusinessType.OTHER)
+    @Log(title = "修改角色数据权限", BusinessType = BusinessType.OTHER)
     @PreAuthorize("@pm.hasPerms('sys:role:dataScope')")
     @PutMapping("/dataScope")
     public AjaxResult updateDataScope(@RequestBody SysRole sysRole) {
